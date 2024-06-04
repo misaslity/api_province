60b018a90be9cd563c0a946b8efc194046705991
NguyenHoangLongVNUK <long.nguyen200406@vnuk.edu.vn>
2024-06-04 16:50:36 +0700

    commit

--- route/route.js
+++ route/route.js
@@ -6,7 +6,7 @@ const get_list_ward = require('../controller/get_ward');
 const router = express.Router();
 
 router.get("/api/v1/province", get_list_province)
-router.get("/api/v1/district/:province_id", get_list_district)
-router.get("/api/v1/ward/:district_id", get_list_ward)
+router.get("/api/v1/province/district/:province_id", get_list_district)
+router.get("/api/v1/province/ward/:district_id", get_list_ward)
 
 module.exports = router;
